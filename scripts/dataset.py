8576035dd6e4a5658f1151391b850e00604e0f26
Hiroki Yokoyama <hiroki.yokoyama@okadanet.org>
2018-11-06 15:54:20 +0900

    update data url

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,19 +1,24 @@
 import os
-import urllib
+from six.moves import urllib
 import tarfile
+import sys
 
-DATA_URL = "https://storage.cloud.google.com/download.tensorflow.org/data/speech_commands_v0.02.tar.gz"
+DATA_URL = "http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz"
 
 def maybe_download_and_extract(data_url, dest_directory):
-    if not os.path.exists(dest_directory):
-        os.makedirs(dest_directory)
+    if os.path.exists(dest_directory):
+        return
+    os.makedirs(dest_directory)
     filename = data_url.split('/')[-1]
     filepath = os.path.join(dest_directory, filename)
     if not os.path.exists(filepath):
         def _progress(count, block_size, total_size):
             percent = float(count * block_size) / float(total_size) * 100.0
-            print '\r>> Downloading %s %.1f%%' % (filename, percent)
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename, percent))
+            sys.stdout.flush()
 
         filepath, _ = urllib.request.urlretrieve(data_url, filepath, _progress)
-      print ''
+        print ''
     tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+    os.remove(filepath)
+
